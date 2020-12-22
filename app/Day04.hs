6d595c3d98bde50fd170354c587e059ee1ae1e92
Philip Weinke <philip.weinke@digitalnoise.de>
2020-12-22 09:04:27 +0100

    Improve day 4

--- app/Day04.hs
+++ app/Day04.hs
@@ -6,9 +6,9 @@ import Data.List.Split
 import qualified Data.Map as Map
 
 passportSchema = Map.fromList [
-  ("byr", between 1920 2002),
-  ("iyr", between 2010 2020),
-  ("eyr", between 2020 2030),
+  ("byr", isBetween 1920 2002),
+  ("iyr", isBetween 2010 2020),
+  ("eyr", isBetween 2020 2030),
   ("hgt", isValidHeight),
   ("hcl", isHexColor),
   ("ecl", isEyeColor),
@@ -16,10 +16,8 @@ passportSchema = Map.fromList [
   ]
 
 toPassport x = Map.fromList (map (\x -> (head x, last x)) (map (splitOn ":") (splitOneOf " \n" x)))
-toPassports x = map toPassport x
 
-between min max v = vi >= min && vi <= max
-  where vi = (read v :: Int)
+isBetween min max v = (read v :: Int) `elem` [min..max]
 
 isHexColor v = hash == '#' && length values == 6 && all isHexDigit values
   where (hash:values) = v
@@ -31,7 +29,7 @@ isPid v = length v == 9 && all isDigit v
 isValidHeight v =
   let unit = dropWhile isDigit v
       value = takeWhile isDigit v
-  in (unit == "cm" && between 150 193 value) || (unit == "in" && between 59 76 value)
+  in (unit == "cm" && isBetween 150 193 value) || (unit == "in" && isBetween 59 76 value)
 
 isFieldValid passport field isValid = case Map.lookup field passport of
   Nothing -> False
@@ -45,7 +43,7 @@ allFieldsValid schema passport = Map.foldrWithKey (\field validator acc -> acc &
 
 solve :: String -> IO ()
 solve input = do
-  let passports = toPassports (splitOn "\n\n" input)
+  let passports = map toPassport (splitOn "\n\n" input)
 
   putStrLn ("Part 1: " ++ (show (countValid (map (hasAllRequiredFields passportSchema) passports))))
   putStrLn ("Part 2: " ++ (show (countValid (map (allFieldsValid passportSchema) passports))))
